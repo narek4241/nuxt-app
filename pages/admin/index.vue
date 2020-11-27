51ec4bc047c0a76cf6d01cbd9ecf57c8762c992d
narek4241 <narekghazaryan02@gmail.com>
2020-11-28 01:16:02 +0400

    added admin, addPost, UI(AppButton, AppControlInput), some changes, (prev. vue warns remained)

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,5 +1,41 @@
 <template>
-  <div>Admin</div>
+  <div class="admin-page">
+    <section class="new-posts">
+      <button @click="linkToNewPost">Create Post</button>
+    </section>
+    <section class="existing-posts">
+      <h1>Existing Posts</h1>
+      <PostList />
+    </section>
+  </div>
 </template>
 
-<style scoped></style>
+<script>
+import PostList from "@/components/Posts/PostList";
+export default {
+  components: PostList,
+
+  methods: {
+    linkToNewPost() {
+      // #task #findOut '$router' and not '$route' (mini)
+      this.$router.push("/admin/new-post");
+    }
+  }
+};
+</script>
+
+<style scoped>
+.admin-page {
+  padding: 20px;
+}
+
+.new-post {
+  text-align: center;
+  border-bottom: 2px solid #ccc;
+  padding-bottom: 10px;
+}
+
+.existing-posts h1 {
+  text-align: center;
+}
+</style>
---
+++ pages/admin/new-post/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="admin-new-post-page">
+    <section class="new-post-form">
+      <form @submit.prevent="onSave">
+        <AppControlInput v-model="editedPost.author"
+          >Author Name</AppControlInput
+        >
+        <AppControlInput v-model="editedPost.title">Title</AppControlInput>
+        <AppControlInput v-model="editedPost.thumbnailLink"
+          >Author Name</AppControlInput
+        >
+        <AppControlInput v-model="editedPost.content" control-type="textarea"
+          >Content</AppControlInput
+        >
+
+        <AppButton type="submit">Save</AppButton>
+        <AppButton type="button" btn-style="cancel" @click="onCancel"
+          >Cancel</AppButton
+        >
+      </form>
+    </section>
+  </div>
+</template>
+
+<script>
+import AppControlInput from "@/components/UI/AppControlInput";
+import AppButton from "@/components/UI/AppButton";
+
+export default {
+  components: { AppControlInput, AppButton },
+
+  data() {
+    return {
+      editedPost: {
+        author: "",
+        title: "",
+        thumbnailLink: "",
+        content: ""
+      }
+    };
+  },
+  methods: {
+    onCancel() {
+      this.$router.push("/admin");
+    },
+
+    resetForm() {
+      this.editedPost.author = "";
+      this.editedPost.title = "";
+      this.editedPost.thumbnailLink = "";
+      this.editedPost.content = "";
+    },
+
+    onSave() {
+      console.log({ ...this.editedPost });
+      this.resetForm();
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@mixin flexCenter($fl-dir: row) {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: $fl-dir;
+}
+
+.admin-new-post-page {
+  @include flexCenter();
+  .new-post-form {
+    width: 60%;
+  }
+}
+</style>
